bcd3193e0a7e917d449a8c96573630b495a9f63f
KaayaRane <63211165+KaayaRane@users.noreply.github.com>
2023-05-07 19:19:57 -0500

    first commit: so far added a Post component and some cool CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from "./Post.js";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {username: "kaaba",
+     caption: "WOW it works",
+     imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
+    },
+    {username: "baaba", 
+     caption: "HOIIII",
+     imageUrl: "https://www.jquery-az.com/html/images/banana.jpg"
+    },
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/*Header*/}
+      <div className="app__header">
+        <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram logo">
+
+        </img>
+      </div>
+
+      <h1>HELLO</h1>
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
+      {/* <Post username= "laaba" caption="HELLO" imageUrl= "https://amymhaddad.s3.amazonaws.com/morocco-blue.png"/> */}
+      {/*Posts*/}
+      {/*Posts*/}
     </div>
   );
 }
---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+//responsible for all of our firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyAGIQChvlp8M-PcgRem1SZIJBmliNTqp_Y",
+    authDomain: "instagram-clone-8990b.firebaseapp.com",
+    databaseURL: "https://instagram-clone-8990b-default-rtdb.firebaseio.com",
+    projectId: "instagram-clone-8990b",
+    storageBucket: "instagram-clone-8990b.appspot.com",
+    messagingSenderId: "596320504399",
+    appId: "1:596320504399:web:f94f02759b741dc4be383a",
+    measurementId: "G-2GTXT2YPZ4"
+  };
+
